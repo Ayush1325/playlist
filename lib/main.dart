cd2480d4c66a64d5cf910d110ed1209cebc946c4
Ayush Singh <ayushsingh1325@gmail.com>
2019-04-17 20:36:37 +0530

    refactor: Moved things around for easier management.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
 import 'package:flutter/material.dart';
-import 'music_item.dart';
-import 'music_provider_bloc.dart';
+import 'shared/blocs/music_provider_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'songs_state.dart';
-import 'songs_provider_event.dart';
+import 'shared/blocs/songs_provider_event.dart';
+import 'song_list.dart';
 
 void main() => runApp(MyApp());
 
@@ -64,26 +63,4 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
 
-}
-
-class SongsListWidget extends StatelessWidget {
-  const SongsListWidget({
-    Key key,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final _musicProviderBloc = BlocProvider.of<MusicProviderBloc>(context);
-    return BlocBuilder(
-      bloc: _musicProviderBloc,
-      builder: (BuildContext context, SongsState state) {
-        return ListView.builder(
-          itemCount: state.songs.length,
-          itemBuilder: (BuildContext context, int index) {
-            return MusicItem(name: state.songs[index]['name'], artist: state.songs[index]['artist'],);
-          },
-        );
-      }
-    );
-  }
 }
---
+++ lib/song_list.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'music_item.dart';
+import 'shared/blocs/music_provider_bloc.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'shared/blocs/songs_state.dart';
+
+class SongsListWidget extends StatelessWidget {
+  const SongsListWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final _musicProviderBloc = BlocProvider.of<MusicProviderBloc>(context);
+    return BlocBuilder(
+        bloc: _musicProviderBloc,
+        builder: (BuildContext context, SongsState state) {
+          return ListView.builder(
+            itemCount: state.songs.length,
+            itemBuilder: (BuildContext context, int index) {
+              return MusicItem(name: state.songs[index]['name'], artist: state.songs[index]['artist'],);
+            },
+          );
+        }
+    );
+  }
+}
